4137284c9dc5f95d0842eca97ea0057efd60c63a
Alvaro <alvaroginerlloret@gmail.com>
2025-07-22 23:25:31 +0200

    project kickoff

--- src/utils/configChecker.ts
+++ src/utils/configChecker.ts
@@ -1,17 +1,20 @@
 import * as fs from 'fs';
 import os from 'os';
 import path from 'path';
+import { confirm } from '@inquirer/prompts';
 
-export function configChecker() {
+export async function configChecker() {
 
 	const homeDir = os.homedir();
 	const configPath = path.join(homeDir, ".config", "clijournal", "userconfig.json");
 	console.log(configPath);
 	if (!fs.existsSync(configPath)) {
-		console.error('No se encontró el archivo de configuración');
-		process.exit(1);
+		const answer = await confirm({ message: "I couldn't find a config file, Do you want to create a default file?" });
+		if (!answer) {
+			console.log("Alright, just remember that the tool won't be able to add entries until you setup your commands");
+		}
 	} else {
-		console.log("Archivo de configuración encontrado");
+		console.log("Creating file...");
 	}
 }
 
