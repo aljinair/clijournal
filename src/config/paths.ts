f140ed40854beb057b6c1df706eeef7d0947d882
Alvaro <alvaroginerlloret@gmail.com>
2025-07-26 20:39:05 +0200

    refactor: Paths.ts adapted to work with PathHandler

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,12 +1,12 @@
 import os from 'os';
 import path from 'path';
+import { PathHandler } from '../classes/PathHandler.ts';
 
 const home = os.homedir();
 
 export const Paths = {
 
-	home,
-	clijournal: path.join(home, "clijournal"),
-	journals: path.join(home, "clijournal", "journals"),
+	clijournal: new PathHandler('clijournal', path.join(home, 'clijournal')),
+	journals: new PathHandler('journals', path.join(home, 'clijournal', 'journals')),
 
 }
